075c8717ba61dee8acfc7b6f687ee1ea077bc82e
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-03-10 11:26:47 -0300

    Refatorando testes

--- arquitetura/test/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_test.dart
+++ arquitetura/test/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_test.dart
@@ -1,23 +1,16 @@
+import 'package:arquitetura/layers/data/datasources/local/get_carros_por_cor_local_datasource_imp.dart';
+import 'package:arquitetura/layers/data/repositories/get_carros_por_cor_repository_imp.dart';
 import 'package:arquitetura/layers/domain/entities/carro_entity.dart';
-import 'package:arquitetura/layers/domain/repositories/get_carros_por_cor_repository.dart';
 import 'package:arquitetura/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase.dart';
 import 'package:arquitetura/layers/domain/usecases/get_carros_por_cor/get_carros_por_cor_usecase_imp.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class GetCarrosPorCorRepositoryImp implements GetCarrosPorCorRepository {
-  @override
-  CarroEntity call(String cor) {
-    if (cor.contains('vermelho')) {
-      return CarroEntity(placa: 'ABC123', qtdPortas: 4, valor: 5000.00);
-    }
-    return CarroEntity(placa: 'QWE', qtdPortas: 2, valor: 2000.00);
-  }
-}
-
 main() {
   test('Deve retornar uma instância de carro quando passado qualquer cor', () {
     GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImp(
-      GetCarrosPorCorRepositoryImp(),
+      GetCarrosPorCorRepositoryImp(
+        GetCarrosPorCorLocalDataSourceImp(),
+      ),
     );
 
     var result = useCase('Azul');
@@ -27,7 +20,9 @@ main() {
 
   test('Deve retornar um carro de 4 portas quando for vermelho', () {
     GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImp(
-      GetCarrosPorCorRepositoryImp(),
+      GetCarrosPorCorRepositoryImp(
+        GetCarrosPorCorLocalDataSourceImp(),
+      ),
     );
 
     var result = useCase('vermelho');
@@ -39,7 +34,9 @@ main() {
       'Deve retornar um carro de 2 portas quando for qualquer cor, exceto vermelho',
       () {
     GetCarrosPorCorUseCase useCase = GetCarrosPorCorUseCaseImp(
-      GetCarrosPorCorRepositoryImp(),
+      GetCarrosPorCorRepositoryImp(
+        GetCarrosPorCorLocalDataSourceImp(),
+      ),
     );
 
     var result = useCase('verde');
