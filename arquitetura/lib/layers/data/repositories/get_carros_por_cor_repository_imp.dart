0c4475240ca8c3e2017dd24d327b213d5df7e1b4
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-03-10 11:12:05 -0300

    Refatorando
    Refatorando a Implementação do Repositório.

---
+++ arquitetura/lib/layers/data/repositories/get_carros_por_cor_repository_imp.dart
@@ -0,0 +1,17 @@
+import 'package:arquitetura/layers/data/datasources/get_carros_por_cor_datasource.dart';
+import 'package:arquitetura/layers/data/dtos/carro_dto.dart';
+import 'package:arquitetura/layers/domain/entities/carro_entity.dart';
+import 'package:arquitetura/layers/domain/repositories/get_carros_por_cor_repository.dart';
+
+class GetCarrosPorCorRepositoryImp implements GetCarrosPorCorRepository {
+  final GetCarrosPorCorDataSource _getCarrosPorCorDataSource;
+
+  GetCarrosPorCorRepositoryImp(this._getCarrosPorCorDataSource);
+
+  @override
+  CarroEntity call(String cor) {
+    // Chamamos a api e ela respondeu um json;
+
+    return _getCarrosPorCorDataSource(cor);
+  }
+}
