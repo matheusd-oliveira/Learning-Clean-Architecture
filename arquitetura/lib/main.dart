7b81bc90f91660918831904643fcf50dde2d72a4
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-03-10 17:17:49 -0300

    Create main.dart

---
+++ arquitetura/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:arquitetura/layers/presentations/ui/pages/carro_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: CarroPage(),
+    ),
+  );
+}
