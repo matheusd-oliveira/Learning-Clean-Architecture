04da0330b9c403c559395e01adf88f931a9a607e
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-03-10 17:46:58 -0300

    Iniciando a injeção
    Iniciando a injeção antes de iniciar o nosso aplicativo.

--- arquitetura/lib/main.dart
+++ arquitetura/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:arquitetura/core/inject/inject.dart';
 import 'package:arquitetura/layers/presentations/ui/pages/carro_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Inject.init();
   runApp(
     MaterialApp(
       home: CarroPage(),
