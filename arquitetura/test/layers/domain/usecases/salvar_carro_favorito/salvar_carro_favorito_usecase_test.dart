0756d62949418a4ffa49139c73094e2f86cbe53c
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-03-14 16:49:16 -0300

    Update salvar_carro_favorito_usecase_test.dart
    refatorando testes

--- arquitetura/test/layers/domain/usecases/salvar_carro_favorito/salvar_carro_favorito_usecase_test.dart
+++ arquitetura/test/layers/domain/usecases/salvar_carro_favorito/salvar_carro_favorito_usecase_test.dart
@@ -1,16 +1,8 @@
+import 'package:arquitetura/layers/data/repositories/salvar_carro_favorito_repository_imp.dart';
 import 'package:arquitetura/layers/domain/entities/carro_entity.dart';
-import 'package:arquitetura/layers/domain/repositories/salvar_carro_favorito_repository.dart';
 import 'package:arquitetura/layers/domain/usecases/salvar_carro_favorito/salvar_carro_favorito_usecase.dart';
 import 'package:arquitetura/layers/domain/usecases/salvar_carro_favorito/salvar_carro_favorito_usecase_imp.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-class SalvarCarroFavoritoRepositoryImp
-    implements SalvarCarroFavoritoRepository {
-  @override
-  Future<bool> call(CarroEntity carroEntity) async {
-    return carroEntity.valor > 0;
-  }
-}
 
 void main() {
   test('Espero que salve o carro com sucesso', () async {
@@ -20,8 +12,10 @@ void main() {
     var carro = CarroEntity(placa: 'placa', qtdPortas: 2, valor: 1);
 
     var result = await useCase(carro);
+    late bool resultExpect;
 
-    expect(result, true);
+    result.fold((l) => null, (r) => resultExpect = r);
+    expect(resultExpect, true);
   });
   test('Espero que não salve o carro quando valor for menor ou igual a 0',
       () async {
@@ -31,7 +25,9 @@ void main() {
     var carro = CarroEntity(placa: 'placa', qtdPortas: 2, valor: 0);
 
     var result = await useCase(carro);
+    late bool resultExpect;
 
-    expect(result, false);
+    result.fold((l) => null, (r) => resultExpect = r);
+    expect(resultExpect, false);
   });
 }
